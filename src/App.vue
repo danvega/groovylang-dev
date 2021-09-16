e97d484c305a5416222114140d913de59b59c9b8
Dan Vega <danvega@gmail.com>
2021-09-16 16:58:25 -0400

    basic landing page welcome

--- src/App.vue
+++ src/App.vue
@@ -2,5 +2,10 @@
 </script>
 
 <template>
-  <h1>Welcome to groovylang.dev</h1>
+  <div class="bg-gray-100"></div>
+  <div class="container mx-auto h-screen flex items-center justify-center">
+    <h1 class="text-4xl text-blue-400 font-semibold">
+      Welcome to groovylang.dev
+    </h1>
+  </div>
 </template>
